311ed56b332a0bf8a5beef88bde07012b64f216b
bmuhamood <bbosa2009@gmail.com>
2020-11-06 16:15:28 -0800

    added methods and contollers actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :find_friendship
 
+    def find_friendship(user_id, friend_id)
+    friendship = Friendship.find_by(user_id: user_id, friend_id: friend_id)
+    friendship = Friendship.find_by(user_id: friend_id, friend_id: user_id) if friendship.nil?
+
+    friendship.nil? ? nil : friendship.id
+  end
+  
   protected
 
   def configure_permitted_parameters
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,20 @@ module ApplicationHelper
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
+    def add_friend_btn(user)
+    return if current_user == user
+    success_btn = 'btn btn-success mr-2'
+    warning_btn = 'btn btn-warning'
+    add_friendship_param = { friendship: { user_id: current_user, friend_id: user, confirmed: false } }
+    delete_param = { id: find_friendship(user.id, current_user.id) }
+
+    if !current_user.friend?(user) && !current_user.request_exists?(user)
+      link_to 'Add as a Friend', friendships_path(add_friendship_param), method: :post, class: success_btn
+    elsif !current_user.request_exists?(user)
+      link_to 'Unfriend', friendship_path(delete_param), method: :delete, class: warning_btn
+    elsif current_user.request_exists?(user)
+      link = capture { link_to 'pending Request. . .', users_path, class: 'disabled btn btn-success mr-2' }
+      link << capture { link_to 'Delete Request', friendship_path(delete_param), method: :delete, class: warning_btn }
+    end
+  end
 end
