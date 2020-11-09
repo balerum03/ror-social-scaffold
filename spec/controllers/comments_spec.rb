6b152d67c7f7c5df3ebcee84c03e06d64e4dba18
balerum03 <adafer123.angra@gmail.com>
2020-11-09 13:22:19 -0600

    Fixed controller spec errors

--- spec/controllers/comments_spec.rb
+++ spec/controllers/comments_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe Comment, type: :model do
   User.delete_all
   Post.delete_all
   let!(:test_comment1) { Comment.create(post_id: nil, user_id: nil, content: nil) }
-  let!(:test_user1) { User.create(email: 'heroku@heroku', name: 'heroku', gravatar_url: 'heroku', password: '123456') }
-  let!(:test_post1) { Post.create(content: 'Test post', user_id: test_user1.id) }
+  let!(:test_user1) { User.create(email: 'test@test.com', name: 'balerum', gravatar_url: 'image_url', password: '123456') }
+  let!(:test_post1) { Post.create(content: 'this is just a test', user_id: test_user1.id) }
 
   it 'Comment creation fails with wrong parameters' do
-    expect(comment1).to be_invalid
+    expect(test_comment1).to be_invalid
   end
 
   it 'Comment creation succeeds with valid parameters' do
-    expect(Comment.create(post_id: test_post1.id, user_id: test_user1.id, content: 'Test comment')).to be_valid
+    expect(Comment.create(post_id: test_post1.id, user_id: test_user1.id, content: 'this is a test')).to be_valid
   end
 end
 # rubocop: enable Layout/LineLength
--- spec/controllers/likes_spec.rb
+++ spec/controllers/likes_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Like, type: :model do
   let!(:test_user1) { User.create(email: 'bbosa2009@gmail.com', name: 'bmuhamood', gravatar_url: 'img_url', password: '20201109') }
   let!(:test_post1) { Post.create(content: 'Test post', user_id: test_user1.id) }
   let!(:test_like1) { Like.create(post_id: nil, user_id: nil) }
-  let!(:test_like2) { Like.create(post_id: post1.id, user_id: test_user1.id) }
+  let!(:test_like2) { Like.create(post_id: test_post1.id, user_id: test_user1.id) }
 
   it 'Checks for a user to exist to create a like.' do
     expect(test_like1).to be_invalid
