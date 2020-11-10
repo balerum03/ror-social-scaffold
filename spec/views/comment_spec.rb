a66c0c6322af26218301da700da8d6b6de6ba0f3
bmuhamood <bbosa2009@gmail.com>
2020-11-10 11:36:57 -0800

    added view test in the rspec

---
+++ spec/views/comment_spec.rb
@@ -0,0 +1,16 @@
+# rubocop: disable Layout/LineLength
+require 'rails_helper'
+
+RSpec.feature 'Comments', type: :feature do
+  login
+
+  scenario 'commenting to a post' do
+    visit posts_path
+    fill_in 'comment_content', with: 'Yes I am'
+    click_on 'Comment'
+
+    expect(page).to have_content 'Comment was successfully created.'
+    expect(page).to have_content "#{@user.name}: Yes I am"
+  end
+end
+# rubocop: enable Layout/LineLength
